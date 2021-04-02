f3419e99b80b025aadb8639dbe907a39041421e0
Coachbre <brekonan1@gmail.com>
2021-04-02 13:01:07 -0400

    added props and state file and created json database

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { Home } from "./Home"
+import { AnimalCard } from "./animal/AnimalCard"
+// import { AnimalProvider } from "./animal/AnimalProvider"
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            {/* Render the location list when http://localhost:3000/ */}
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <Route path="/animals">
+              <AnimalCard />
+            </Route>
+        </>
+    )
+}
