6026923d5cc577758b76a64243f7d709a3493c6c
Coachbre <brekonan1@gmail.com>
2021-04-05 13:04:07 -0400

    edited applicationViews.js and added AnimalManager.js

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,10 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalCard } from "./animal/AnimalCard"
+import { EmployeeCard } from "./employee/Employee"
+import { CustomerCard } from "./customer/Customer"
+import { LocationCard } from "./location/Location"
+
 // import { AnimalProvider } from "./animal/AnimalProvider"
 
 export const ApplicationViews = () => {
@@ -14,7 +18,19 @@ export const ApplicationViews = () => {
 
             {/* Render the animal list when http://localhost:3000/animals */}
             <Route path="/animals">
-              <AnimalCard />
+              <AnimalList />
+            </Route>
+
+            <Route path="/employees">
+                <EmployeeCard />
+            </Route>
+
+            <Route path="/customers">
+                <CustomerCard />
+            </Route>
+
+            <Route path="/locations">
+                <LocationCard />
             </Route>
         </>
     )
